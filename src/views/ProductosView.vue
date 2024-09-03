44eaff76057e8c30730b875c160bc0705894ddc1
Mauricio Fuentes <mauriciofb@gmail.com>
2024-09-03 15:59:05 -0400

    refactor componente carta

--- src/views/ProductosView.vue
+++ src/views/ProductosView.vue
@@ -3,23 +3,12 @@
   <h1 class=" text-center">Productos</h1>
   <div class="row">
     <div class="col-sm-4 col-12 px-5 mb-4">
-      <div class="card">
-        <img src="../assets/img/colaciones.jpg" class="card-img-top" alt="colaciones" />
-        <div class="card-body">
-          <h5 class="card-title fw-bold">{{ nombre }}</h5>
-          <p class="card-text">
-            {{ descripcion }}
-          </p>
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">Precio: {{ precio }}</li>
-          <li class="list-group-item">Cantidad: {{ cantidad }}</li>
-        </ul>
-        <div class="card-body">
-          <a href="#" class="card-link">Ver más</a>
-          <a href="#" class="card-link">Comprar</a>
-        </div>
-      </div>
+      <SingleCard
+        :nombre="nombre"
+        :descripcion="descripcion"
+        :precio="precio"
+        :cantidad="cantidad"
+      />
     </div>
   </div>
   <div class="row">
@@ -28,11 +17,16 @@
 </section>
 </template>
 <script>
+import SingleCard from '../components/SingleCard.vue'
+
 export default {
   methods: {
     VolverAlInicio() {
       this.$router.push({ name: 'inicio' })
     }
+  },
+  components: {
+    SingleCard
   },
   props: {
     nombre: {
